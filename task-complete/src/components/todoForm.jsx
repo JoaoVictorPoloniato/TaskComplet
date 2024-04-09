ab8b9fc77ff71223f880810cd6eb867b55a4f599
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-04-09 13:47:35 -0400

    Atualizado meu todoForm
    Update in the todoForm create the options for select, in the script, but i don't finalized.

--- task-complete/src/components/todoForm.jsx
+++ task-complete/src/components/todoForm.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 
 function TodoForm() {
   return (
-    <div>todoForm</div>
+    <div className="todo-form">
+      <h2>Criar tarefa</h2>
+      <form>
+        <input type="text" placeholder="Digite o Título" />
+        <select>
+          <option value="">Selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
+          <option value="Exercicios">Exercicios</option>
+        </select>
+        <button type="submit">Criar Tarefa</button>
+      </form>
+    </div>
   );
 }
 
