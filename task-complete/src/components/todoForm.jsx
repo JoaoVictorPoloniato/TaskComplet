80ddb8af973fb936c7759314aed19da7205ea735
Joao Victor Poloniato Buss <150697033+JoaoVictorPoloniato@users.noreply.github.com>
2024-04-10 10:34:21 -0400

    Criação de values, inputs e categorys
    Create in the script valuer for aplicate in the form my todos, but i don't finalized now

--- task-complete/src/components/todoForm.jsx
+++ task-complete/src/components/todoForm.jsx
@@ -1,13 +1,35 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
-function TodoForm() {
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!value || !category)
+        return;
+    addTodo(value, category);
+    setValue("");
+    setCategory("");
+    console.log(value, category);
+  };
+
   return (
     <div className="todo-form">
       <h2>Criar tarefa</h2>
-      <form>
-        <input type="text" placeholder="Digite o Título" />
-        <select>
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder="Digite o Título" 
+        value={value}
+        onChange={(e) => setValue(e.target.value)} 
+        />
+        <select 
+         value={category}
+         onChange={(e) => setCategory(e.target.value)}
+         >
           <option value="">Selecione uma categoria</option>
           <option value="Trabalho">Trabalho</option>
           <option value="Pessoal">Pessoal</option>
